83a5f4182bbf6b0837db8930c36009f8f41ee1fd
Negi, Ashish <Ashish.Negi@united.com>
2019-01-08 13:15:31 -0600

    Updating for other versions

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-$( document ).ready(function() {
-    $("#btn-go").click(function () { 
-        var url = "https://qa7.united.com/ual/" + $('#lang-selector').val() + "/widget/bookingwidget";
+$(document).ready(function () {
+    $("#btn-go").click(function () {
+        var url = $('#environment-selector').val() + $('#lang-selector').val() + "/widget/bookingwidget" + $('#version-selector').val();
+        switch ($('#version-selector').val()) {
+            case "?version=1":
+                $("#widget-iframe").height(375);
+                $("#widget-iframe").width(380);
+                break;
+            case "?version=2":
+                $("#widget-iframe").height(404);
+                $("#widget-iframe").width(776);
+                break;
+            case "?version=3":
+                $("#widget-iframe").height(822);
+                $("#widget-iframe").width(300);
+                break;
+            default:
+                $("#widget-iframe").height(375);
+                $("#widget-iframe").width(380);
+        }
         $("#widget-iframe").attr("src", url);
     });
 });
